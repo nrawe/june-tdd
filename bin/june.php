ba869657b80863dddcff4e5ed4db9ad7ec09ff9d
Nick Rawe <nrawe@reapit.com>
2018-01-09 20:01:06 +0000

    Move the bootstrap file

--- bin/june.php
+++ bin/june.php
@@ -1,11 +1,10 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use League\CLImate\CLImate;
 use June\Framework\{Loader, Harness, Runner};
 use function June\harness;
-
 
 $runner  = new Runner(new CLImate);
 $harness = harness(new Harness(new Loader, $runner));
