3e0df38287c1e0d859aa5e993032d1bda90c11e0
Nick Rawe <nrawe@reapit.com>
2018-01-09 20:38:34 +0000

    Add a helper for parameterising strings

--- library/helpers.php
+++ library/helpers.php
@@ -11,3 +11,14 @@ function ends_with(string $haystack, string $needle): bool
 {
     return substr($haystack, -strlen($needle)) === $needle;
 }
+
+function parameterise_string(string $message, array $params): string
+{
+    $parameterised = $message;
+
+    foreach ($params as $param => $value) {
+        $parameterised = str_replace('%' . $param, $value, $parameterised);
+    }
+
+    return $parameterised;
+}
