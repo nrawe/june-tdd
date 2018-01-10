06a6be11b0cf0a9fecb0b1ba1129733792724d83
Nick Rawe <nrawe@reapit.com>
2018-01-10 09:48:19 +0000

    Documentation

--- library/Unit.php
+++ library/Unit.php
@@ -5,33 +5,59 @@ namespace June\Framework;
 use Countable;
 use June\Framework\Contracts\Step;
 
+/**
+ * A Unit represents a class that needs to be tested.
+ * 
+ * It is comprised of a series of Steps which comprise the actual steps.
+ */
 class Unit implements Countable
 {
+    /**
+     * The name of the Unit being tested.
+     * 
+     * @var string
+     */
     protected $name;
 
-    protected $steps;
+    /**
+     * The Steps which comprise the Unit.
+     * 
+     * @var Step[]
+     */
+    protected $steps = [];
 
     public function __construct(string $name)
     {
-        $this->steps = [];
         $this->name  = $name;
     }
 
+    /**
+     * Adds a Step into the Unit.
+     */
     public function add(Step $step): void
     {
         $this->steps[] = $step;
     }
 
+    /**
+     * Returns the number of Steps that have been added to the Unit.
+     */
     public function count(): int
     {
         return count($this->steps);
     }
 
+    /**
+     * Returns the name of the Unit.
+     */
     public function name(): string
     {
         return $this->name;
     }
 
+    /**
+     * Returns an array of Steps which the Unit is comprised of.
+     */
     public function steps(): array
     {
         return $this->steps;
