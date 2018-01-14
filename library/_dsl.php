643c194a87c7400acd48bef0041ef9456fd213c2
Nick Rawe <nrawe@reapit.com>
2018-01-14 20:18:29 +0000

    Tidy up the DSL layer

--- library/_dsl.php
+++ library/_dsl.php
@@ -2,10 +2,13 @@
 
 namespace June;
 
+use June\Framework\Factories\HarnessFactory;
 use June\Framework\Runtime\Harness;
 use RuntimeException;
-use June\Framework\Factories\HarnessFactory;
 
+/**
+ * Returns the current harness instance.
+ */
 function harness(): Harness
 {
     static $instance;
@@ -17,21 +20,47 @@ function harness(): Harness
     return $instance;
 }
 
-function bug(string $name, callable $case)
+/**
+ * Registers a testing step in the form of a bug.
+ * 
+ * Bug steps are semantically differentiated from tests to separate out
+ * design from reality.
+ * 
+ * The $step callback will be executed when the framework reaches the step.
+ */
+function bug(string $name, callable $steps)
 {
-    harness(null)->test($name, $case);
+    harness(null)->test($name, $steps);
 }
 
-function test(string $name, callable $case)
+/**
+ * Registers a testing step in the form of a test.
+ * 
+ * Test steps are semantically differentiated from bugs to separate out
+ * reality from design.
+ * 
+ * The $step callback will be executed when the framework reaches the step.
+ */
+function test(string $name, callable $steps)
 {
-    harness()->test($name, $case);
+    harness()->test($name, $steps);
 }
 
+/**
+ * When `psy/psysh` is installed, this function can be used to drop into a
+ * terminal to debug failing test steps.
+ */
 function tinker(array $context = [])
 {
     \Psy\debug($context);
 }
 
+/**
+ * Registers a unit to be tested.
+ * 
+ * The $tests callback will be invoked immediately and should in turn register
+ * any required test steps for the unit with the framework.
+ */
 function unit(string $name, callable $tests)
 {
     harness()->unit($name);
@@ -39,12 +68,30 @@ function unit(string $name, callable $tests)
     $tests();
 }
 
-function xbug(string $name, callable $case)
+/**
+ * Registers a disabled testing step in the form of a bug.
+ * 
+ * Bug steps are semantically differentiated from tests to separate out
+ * design from reality.
+ * 
+ * The $step callback will not be executed and as such this helper can be used
+ * to temporarily disable a failing test step.
+ */
+function xbug(string $name, callable $steps)
 {
     harness()->skipped($name, $case);
 }
 
-function xtest(string $name, callable $case)
+/**
+ * Registers a disabled testing step in the form of a test.
+ * 
+ * Test steps are semantically differentiated from bugs to separate out
+ * reality from design.
+ * 
+ * The $step callback will not be executed and as such this helper can be used
+ * to temporarily disable a failing test step.
+ */
+function xtest(string $name, callable $steps)
 {
     harness()->skipped($name, $case);
 }
